a133ceb322d813de56dfe184c332455dc7b61700
skdbsp123 <skdbsp123@gmail.com>
2019-10-17 23:42:46 +0530

    my commit

---
+++ abc.cpp
@@ -0,0 +1,13 @@
+/* this program adds two numbers */
+# include <iostream>
+int main(){
+using namespace std;
+float a,b,c;
+cout<<"enter value of a=";
+cin>>a;
+cout<<"enter value of b=";
+cin>>b;
+c=a+b;
+cout<<"the sum is :"<<c;
+return 0;
+}
